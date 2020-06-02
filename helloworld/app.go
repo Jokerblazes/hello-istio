0de4c1165f34268044f79d7f5ebc862b4682ab30
joker <cdzheng@thoughtworks.com>
2020-06-02 14:55:27 +0800

    modify app

--- helloworld/app.go
+++ helloworld/app.go
@@ -9,11 +9,17 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("hello world v2!")
+	json.NewEncoder(w).Encode("hello world!")
 }
+
+func hiWorld(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("hi world!")
+}
+
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", helloWorld).Methods("GET")
+	router.HandleFunc("/hi", hiWorld).Methods("GET")
 	log.Fatal(http.ListenAndServe(":6000", router))
 }
